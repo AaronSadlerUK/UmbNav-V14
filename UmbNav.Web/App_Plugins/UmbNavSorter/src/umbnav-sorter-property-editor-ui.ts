bde02946c288d159075a49c95021826310f0997f
Aaron Sadler <aaron.sadler@moriyama.co.uk>
2024-10-26 14:12:07 +0100

    Attempt to use the link picker modal
    BROKEN!

--- UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-sorter-property-editor-ui.ts
+++ UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-sorter-property-editor-ui.ts
@@ -1,13 +1,22 @@
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-import { css, html, customElement, LitElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, customElement, LitElement, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/extension-registry";
+import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
+// import { UMB_MODAL_MANAGER_CONTEXT, UmbModalManagerContext } from "@umbraco-cms/backoffice/modal";
+// import { UMB_LINK_PICKER_MODAL } from "@umbraco-cms/backoffice/multi-url-picker";
+import { UMB_LINK_PICKER_MODAL } from '@umbraco-cms/backoffice/multi-url-picker';
+import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
+import { UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-editor";
+import { extractUmbColorVariable } from "@umbraco-cms/backoffice/resources";
 import { Guid } from "guid-typescript";
 
 import './umbnav-group.js';
 import type { UmbNavGroup, ModelEntryType } from './umbnav-group.js';
 
 @customElement('umbnav-sorter-property-editor-ui')
-export default class UmbNavSorterPropertyEditorUIElement extends LitElement implements UmbPropertyEditorUiElement {
+export class UmbNavSorterPropertyEditorUIElement extends UmbElementMixin(LitElement) implements UmbPropertyEditorUiElement {
+	// private _modalContext?: UmbModalManagerContext;
+
 	@property({ type: Array })
     groupOneItems: ModelEntryType[] = [
 		{
@@ -191,9 +200,47 @@ export default class UmbNavSorterPropertyEditorUIElement extends LitElement impl
 		},
 	];
 
+	@property({ type: String })
+	public set value(value: string) {
+		this._value = value;
+		if(this.value != "" && this.value != undefined) {
+			var stringArray = this.value.split(/(\s+)/);
+			this.icon = stringArray[0];
+			if(stringArray.length > 1) {
+				this.color = stringArray[2];
+			}
+		}
+	}
+	public get value(): string {
+		return this._value;
+	}
+	_value = '';
+
+	@state()
+	color = '';
+
+	@state()
+	icon = '';
+
+	constructor() {
+		super();
+		this.consumeContext(UMB_MODAL_MANAGER_CONTEXT, (_instance) => {
+			// this._modalContext = _instance;
+		});
+	}
+
     render() {
         return html`
+			<div>
+<uui-icon name="${this.icon}" style="color:var(${extractUmbColorVariable(this.color)})"></uui-icon>
+<pre>${this.icon} ${this.color}</pre>
+</div>
+			<uui-button look="primary"
+			color="default"
+			label="Pick an icon"
+			@click=${this._OpenIconPicker}></uui-button>
 
+			<umb-property-editor-ui-multi-url-picker></umb-property-editor-ui-multi-url-picker>
 <div class="outer-wrapper">
 					<umbnav-group
 						.value=${this.groupOneItems}
@@ -213,6 +260,27 @@ export default class UmbNavSorterPropertyEditorUIElement extends LitElement impl
 			}
 		`,
 	];
+
+	async _OpenIconPicker() {
+		// const modal = UMB_LINK_PICKER_MODAL;
+		// const pickerContext = this._modalContext?.open(this, modal);
+		const modalManager = await this.getContext(UMB_MODAL_MANAGER_CONTEXT);
+		const modalHandler = modalManager.open(this, UMB_LINK_PICKER_MODAL);
+		// const data = await pickerContext?.onSubmit();
+		// if (!data) return;
+		// if (data.color) {
+		// 	this.color = data.color as string;
+		// }
+		// this.icon = data.icon as string;
+
+		if (!modalHandler) return;
+		this.#dispatchChangeEvent();
+	}
+
+	#dispatchChangeEvent(){
+		this.value = this.icon + " " + this.color;
+		this.dispatchEvent(new UmbPropertyValueChangeEvent());
+	}
 }
 
 declare global {
