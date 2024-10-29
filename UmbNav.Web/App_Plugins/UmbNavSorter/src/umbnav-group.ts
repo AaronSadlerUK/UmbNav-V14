f06f5d184d9109202bfa8ba5b4f9328635be8530
Aaron Sadler <aaron@umbhost.net>
2024-10-29 20:34:19 +0000

    Changed definitions

--- UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-group.ts
+++ UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-group.ts
@@ -20,18 +20,18 @@ import {UmbPropertyValueChangeEvent} from "@umbraco-cms/backoffice/property-edit
 export type ModelEntryType = {
     key: string | null | undefined;
     name: string | null | undefined;
-    description: string | null | undefined,
+    description?: string | null | undefined,
     url: string | null | undefined,
     icon: string | null | undefined,
     itemType: UmbLinkPickerLinkType | null | undefined,
     udi: string | null | undefined,
     anchor: string | null | undefined,
     published: boolean | null | undefined,
-    naviHide: boolean | null | undefined,
-    culture:  | null | undefined,
-    id: number,
+    naviHide?: boolean | null | undefined,
+    culture?: string | null | undefined,
+    id?: number | null | undefined,
     children?: ModelEntryType[];
-    expanded: boolean;
+    expanded?: boolean;
     target?: string | null | undefined
 };
 
