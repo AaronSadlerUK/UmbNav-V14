a2abccac4f1909fe505d3798665cf67a40826b23
Aaron Sadler <aaron@umbhost.net>
2024-11-02 16:39:40 +0000

    Wired up to backoffice

--- UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-sorter-property-editor-ui.ts
+++ UmbNav.Web/App_Plugins/UmbNavSorter/src/umbnav-sorter-property-editor-ui.ts
@@ -6,198 +6,21 @@ import {
     UmbPropertyValueChangeEvent
 } from "@umbraco-cms/backoffice/property-editor";
 import {UmbElementMixin} from "@umbraco-cms/backoffice/element-api";
-import {Guid} from "guid-typescript";
 import './umbnav-group.js';
 import type {UmbNavGroup, ModelEntryType} from './umbnav-group.js';
 
 @customElement('umbnav-sorter-property-editor-ui')
 export class UmbNavSorterPropertyEditorUIElement extends UmbElementMixin(LitElement) implements UmbPropertyEditorUiElement {
+    @property()
+    value: ModelEntryType[] | undefined;
 
-    @property({type: Array})
-    groupOneItems: ModelEntryType[] = [
-        {
-            key: Guid.create().toString(),
-            name: 'Apple',
-            description: 'https://www.apple.com/',
-            url: 'https://www.apple.com/',
-            icon: 'icon-smiley color-pink',
-            itemType: "external",
-            udi: null,
-            anchor: '',
-            published: true,
-            naviHide: false,
-            culture: null,
-            target: '_blank',
-            id: 1234,
-            children: [
-                {
-                    key: Guid.create().toString(),
-                    name: 'Juice',
-                    description: '/juice/',
-                    url: '/juice/',
-                    icon: 'icon-document',
-                    itemType: "document",
-                    udi: 'umb://document/1234567890',
-                    anchor: '',
-                    published: true,
-                    naviHide: false,
-                    culture: null,
-                    id: 1234,
-                    children: [
-                        {
-                            key: Guid.create().toString(),
-                            name: 'Juice 2',
-                            description: '/juice-2/',
-                            url: '/juice-2/',
-                            icon: 'icon-document',
-                            itemType: "document",
-                            udi: 'umb://document/1234567890',
-                            anchor: '',
-                            published: true,
-                            naviHide: false,
-                            culture: null,
-                            id: 1234,
-                            children: [],
-                            expanded: false
-                        },
-                        {
-                            key: Guid.create().toString(),
-                            name: 'Milk',
-                            description: '/milk/',
-                            url: '/milk/',
-                            icon: 'icon-document',
-                            itemType: "document",
-                            udi: 'umb://document/1234567890',
-                            anchor: '',
-                            published: true,
-                            naviHide: false,
-                            culture: null,
-                            id: 1234,
-                            children: [],
-                            expanded: false
-                        },
-                    ],
-                    expanded: false
-                },
-                {
-                    key: Guid.create().toString(),
-                    name: 'Milk 2',
-                    description: '/milk-2/',
-                    url: '/milk-2/',
-                    icon: 'icon-document',
-                    itemType: "document",
-                    udi: 'umb://document/1234567890',
-                    anchor: '',
-                    published: true,
-                    naviHide: false,
-                    culture: null,
-                    id: 1234,
-                    children: [],
-                    expanded: false
-                },
-            ],
-            expanded: false
-        },
-        {
-            key: Guid.create().toString(),
-            name: 'Banana',
-            description: '/banana/',
-            url: '/banana/',
-            icon: 'icon-document',
-            itemType: "document",
-            udi: 'umb://document/1234567890',
-            anchor: '',
-            published: true,
-            naviHide: false,
-            culture: null,
-            id: 1234,
-            children: [],
-            expanded: false
-        },
-        {
-            key: Guid.create().toString(),
-            name: 'Pear',
-            description: '/pear/',
-            url: '/pear/',
-            icon: 'icon-document',
-            itemType: "document",
-            udi: 'umb://document/1234567890',
-            anchor: '',
-            published: true,
-            naviHide: false,
-            culture: null,
-            id: 1234,
-            children: [],
-            expanded: false
-        },
-        {
-            key: Guid.create().toString(),
-            name: 'Pineapple',
-            description: '/pineapple/',
-            url: '/pineapple/',
-            icon: 'icon-document',
-            itemType: "document",
-            udi: 'umb://document/1234567890',
-            anchor: '',
-            published: true,
-            naviHide: false,
-            culture: null,
-            id: 1234,
-            children: [],
-            expanded: false
-        },
-        {
-            key: Guid.create().toString(),
-            name: 'Lemon',
-            description: '/lemon/',
-            url: '/lemon/',
-            icon: 'icon-document',
-            itemType: "document",
-            udi: 'umb://document/1234567890',
-            anchor: '',
-            published: true,
-            naviHide: false,
-            culture: null,
-            id: 1234,
-            children: [
-                {
-                    key: Guid.create().toString(),
-                    name: 'Cola',
-                    description: '/cola/',
-                    url: '/cola/',
-                    icon: 'icon-document',
-                    itemType: "document",
-                    udi: 'umb://document/1234567890',
-                    anchor: '',
-                    published: true,
-                    naviHide: false,
-                    culture: null,
-                    id: 1234,
-                    children: [],
-                    expanded: false
-                },
-                {
-                    key: Guid.create().toString(),
-                    name: 'Pepsi',
-                    description: '/pepsi/',
-                    url: '/pepsi/',
-                    icon: 'icon-document',
-                    itemType: "document",
-                    udi: 'umb://document/1234567890',
-                    anchor: '',
-                    published: true,
-                    naviHide: false,
-                    culture: null,
-                    id: 1234,
-                    children: [],
-                    expanded: false
-                },
-            ],
-            expanded: false
-        },
-    ];
     @property({ attribute: false })
     config: UmbPropertyEditorConfigCollection | undefined;
+
+    private onChange(e: Event) {
+        this.value = (e.target as UmbNavGroup).value;
+        this.dispatchEvent(new UmbPropertyValueChangeEvent());
+    }
 
     render() {
         return html`
