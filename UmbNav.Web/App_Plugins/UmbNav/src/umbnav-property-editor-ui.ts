2c99ab63f45034d1f015545aa87d7734d141e44b
Aaron Sadler <aaron@umbhost.net>
2024-11-02 17:29:42 +0000

    Added Toggle All Button

--- UmbNav.Web/App_Plugins/UmbNav/src/umbnav-property-editor-ui.ts
+++ UmbNav.Web/App_Plugins/UmbNav/src/umbnav-property-editor-ui.ts
@@ -17,14 +17,41 @@ export class UmbNavSorterPropertyEditorUIElement extends UmbElementMixin(LitElem
     @property({ attribute: false })
     config: UmbPropertyEditorConfigCollection | undefined;
 
+    @property({type: Boolean, attribute: false})
+    public get enableToggleAllButton(): Boolean {
+        return <Boolean>this.config?.find(item => item.alias === 'enableToggleAllButton')?.value ?? false;
+    }
+
     private onChange(e: Event) {
         this.value = (e.target as UmbNavGroup).value;
         this.dispatchEvent(new UmbPropertyValueChangeEvent());
     }
 
+    toggleAllNodes() {
+        const expand = this.value?.some(item => item.expanded);
+
+        const toggleRecursive = (items: ModelEntryType[] = []): ModelEntryType[] => {
+            return items.map(item => {
+                const newItem = { ...item, expanded: !expand };
+                if (newItem.children && newItem.children.length > 0) {
+                    newItem.children = toggleRecursive(newItem.children);
+                }
+                return newItem;
+            });
+        };
+
+        this.value = toggleRecursive(this.value);
+        this.requestUpdate();
+    }
+
     render() {
         return html`
             <div class="outer-wrapper">
+                ${this.enableToggleAllButton ? html`
+                    <uui-button label="Toggle All Items" look="secondary"
+                                @click=${() => this.toggleAllNodes()}
+                    ></uui-button>
+                ` : ''}
                 <umbnav-group
                         .config=${this.config}
                         .value=${this.value}
@@ -38,7 +65,7 @@ export class UmbNavSorterPropertyEditorUIElement extends UmbElementMixin(LitElem
         css`
             .outer-wrapper {
                 display: grid;
-                gap: var(--uui-size-layout-1);
+                gap: 4px;
             }
         `,
     ];
